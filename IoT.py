97748bc086d23d847e9d0a43544311447ea5e89b
maxbe <maxben708@gmail.com>
2019-04-08 01:41:02 +0300

    Changed request handling.

--- IoT.py
+++ IoT.py
@@ -1,18 +1,18 @@
 import cv2
 # TODO : think about another Thread
 import socket
+import requests
 
-UDP_IP = "192.168.31.62"
-UDP_PORT = 5005
-MAX_NUM_CONNECTIONS = 20
+RECEIVER_IP = "192.168.31.62"
+RECEIVER_PORT = -1
 VIDEO_FILE_PATH = "video_ready_5.mp4"
 ENCODE_PARAMS = [int(cv2.IMWRITE_JPEG_QUALITY), 40]
 
-if __name__ == '__main__':
+
+def start_video_stream(sock):
     cap = cv2.VideoCapture(VIDEO_FILE_PATH)
-    print("Waiting connections...")
+    i = 0
     try:
-        connection = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         while True:
             ret, frame = cap.read()
             if not ret:
@@ -20,6 +20,31 @@ if __name__ == '__main__':
                 continue
             frame = cv2.resize(frame, (266, 200))
             data_str = cv2.imencode('.jpg', frame, ENCODE_PARAMS)[1].tobytes()
-            connection.sendto(data_str, (UDP_IP, UDP_PORT))
+            sock.sendto(data_str, (RECEIVER_IP, RECEIVER_PORT))
+
+            i += 1
+
+            if i % 30 == 0:
+                print("Sent ", i, " packets")
     finally:
-        connection.close()
+        sock.close()
+
+
+if __name__ == '__main__':
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    print("Recovering stream...")
+    res = requests.post("http://{}:{}/api/Stream/start-stream".format(RECEIVER_IP, 5000), params={
+        'driverIdentifier': "1"
+    }, timeout=60000)
+
+    print("type(res.status_code) = ", type(res.status_code))
+    status = int(res.status_code)
+    if status == 200:
+        print("Stream started!")
+        RECEIVER_PORT = int(res.content)
+        print("Sending to port ", RECEIVER_PORT)
+        start_video_stream(sock)
+    else:
+        print("received: {}".format(res.content))
+        print("code: {}".format(status))
+        exit(1)
